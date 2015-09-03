14b794b862c667bff158ecd627ea73dc90b3796b
wtnabe <wtnabe@gmail.com>
2015-09-03 19:22:29 +0900

    sprout _raw_get from GAShikomi::Subcommand::Ga#get and make _raw_get recursive callable

--- lib/ga_shikomi/subcommand/ga.rb
+++ lib/ga_shikomi/subcommand/ga.rb
@@ -8,6 +8,25 @@ module GAShikomi
       # --end-date
       # --metrics
       def get(config)
+        result = _raw_get(config)
+
+        case result['rows'].size
+        when 1
+          [result['totalsForAllResults']]
+        else
+          fields = %w(columnHeaders totalsForAllResults rows)
+          fields.map {|e| {e => result[e]}}.reduce(:merge)
+        end
+
+      end
+
+      #
+      # allow recursive call
+      #
+      # [param]  Hash
+      # [return] Hash
+      #
+      def _raw_get(config)
         opts = {
           'ids'        => options['profile_id'],
           'start-date' => options['start_date'],
@@ -30,13 +49,20 @@ module GAShikomi
 
         result = api.execute(api.analytics.data.ga.get, opts)
 
-        case result['rows'].size
-        when 1
-          [result['totalsForAllResults']]
-        else
-          fields = %w(columnHeaders totalsForAllResults rows)
-          fields.map {|e| {e => result[e]}}.reduce(:merge)
+        if result['nextLink']
+          query = result['query']
+          start = query['start-index']
+          limit = query['max-results']
+
+          rows  = result['rows']
+
+          config['start-index'] = start + limit
+
+          result = _raw_get(config)
+          result['rows'] += rows
         end
+
+        result
       end
     end
   end
